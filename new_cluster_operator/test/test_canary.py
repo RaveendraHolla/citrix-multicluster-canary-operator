1ce87cc9c40c4469d79f89beda801b9d05052de0
Raveendra Holla <raveendra.holla2@citrix.com>
2020-10-17 18:03:02 +0530

    Name changes.

--- new_cluster_operator/test/test_canary.py
+++ new_cluster_operator/test/test_canary.py
@@ -11,7 +11,7 @@ class CanaryTestCase(unittest.TestCase):
     @mock.patch('canary.read_existing_gtp')
     @mock.patch('canary.apply_gtp')
     @mock.patch('canary.calculate_health_score')
-    def test_handle_multicluster_canary_crd(self, mock_calculate_health_score, mock_apply_gtp, mock_read_gtp):
+    def test_handle_canary_crd(self, mock_calculate_health_score, mock_apply_gtp, mock_read_gtp):
         with open("gtp_cr.yaml") as f:
             gtp_dict = yaml.load(f, Loader=yaml.FullLoader)
         mock_read_gtp.return_value = gtp_dict
@@ -19,10 +19,10 @@ class CanaryTestCase(unittest.TestCase):
         with open("canary_cr.yaml") as f:
             canary_dict = yaml.load(f, Loader=yaml.FullLoader)
         mock_calculate_health_score.return_value = 95
-        assert(canary.handle_multicluster_canary_crd(canary_dict) is True)
+        assert(canary.handle_canary_crd(canary_dict) is True)
         mock_calculate_health_score.return_value = 85
         mock_read_gtp.return_value = gtp_dict_original
-        assert(canary.handle_multicluster_canary_crd(canary_dict) is False)
+        assert(canary.handle_canary_crd(canary_dict) is False)
 
 if __name__ == '__main__':
     unittest.main()
